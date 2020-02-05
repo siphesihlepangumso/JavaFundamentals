cfd0a6eb2e377de10e5e45f423b2c88a72dd77ea
siphesihlepangumso <siphesihle.pangumso@ilabquality.com>
2020-02-05 09:38:45 +0200

    Total Price fix, now displays at the end.

--- src/com/ilabquality/day2/SalesCalculation.java
+++ src/com/ilabquality/day2/SalesCalculation.java
@@ -2,26 +2,36 @@ package com.ilabquality.day2;
 
 import java.util.Scanner;
 
+
+/**
+ *
+ * @author <Siphesihle Pangumso>
+ */
 public class SalesCalculation {
 
     public static void main(String[] a) {
 
-        //Declare variables
+        //variable declaration
         int productID, quantity;
         double price = 0, totalPrice = 0;
 
-        //input from user
+        //user input - product number
         System.out.print("Whats the product number of the item you want? (HINT: Type '-999' to exit): ");
         productID = new Scanner(System.in).nextInt();
 
+        //Program logic
         while (productID != -999) {
 
+            //correct product number validation
             if (productID > 0 && productID < 6) {
-                //input from user
+
+                //user input - number of products
                 System.out.print("How many items of product " + productID + " would you like?: ");
                 quantity = new Scanner(System.in).nextInt();
 
+                //validate quantity of items
                 if (quantity > 0) {
+
                     switch (productID) {
                         case 1:
                             price = 2.98 * quantity;
@@ -40,24 +50,30 @@ public class SalesCalculation {
                             break;
                     }
 
-                    //sum up if product ID is known
+                    //Calculate total price
                     totalPrice = totalPrice + price;
 
                 } else {
-                    System.out.println("Please enter a valid number of items for product " + productID);
+                    System.out.println("Invalid number of items for product " + productID + ". Try again...");
                     continue;
                 }
 
-                //Display output
-                System.out.printf("Total retail price is R%.2f", totalPrice).println();
+                //Display price of current item(s)
+                System.out.printf("Price of product " + productID + " is R%.2f", price).println();
 
             } else {
                 System.out.println("PRODUCT NUMBER UNKNOWN!");
             }
 
+            //user input - product number (within loop)
             System.out.print("Whats the product number of the item you want? (HINT: Type '-999' to exit): ");
             productID = new Scanner(System.in).nextInt();
         }
+
+        if (totalPrice != 0) {
+            System.out.printf("Total retail price is R%.2f", totalPrice).println();
+        }
+
         System.out.println("PROGRAM EXITING...");
     }
 }
